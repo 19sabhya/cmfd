4b30fb4eca04ddc088b31cdceff0b2ae107a28de
Sabhya A. Lokhande <64120740+19sabhya@users.noreply.github.com>
2021-05-15 15:59:08 +0530

    Update ela_extractor.py

--- src/ela_extractor.py
+++ src/ela_extractor.py
@@ -4,31 +4,37 @@ import numpy as np
 import math
 
 def getImageDifference(image1, image2):
-    height, width = image1.shape[:2]
-    outputMap = np.zeros((3, width, height))
-
-    for i in range(0, width):
-        for j in range (0, height):
-            tmpColor1 = image1[i][j]
-            tmpColor2 = image2[i][j]
-            red_diff = int(tmpColor1[0]) - int(tmpColor2[0])
-            green_diff = int(tmpColor1[1]) - int(tmpColor2[1])
-            blue_diff = int(tmpColor1[2]) - int(tmpColor2[2])
-            outputMap[0][i][j] = float(red_diff * red_diff)
-            outputMap[1][i][j] = float(green_diff * green_diff)
-            outputMap[2][i][j] = float(blue_diff * blue_diff)
+    width, height = image1.shape[1::-1]
+    outputMap = np.zeros((height, width, 3))
+    tmpColor1 = np.zeros(3)
+    
+    for i in range(0, height):
+      # print('i: ', i)
+      for j in range (0, width):
+          #print('i, j: ', i, j)
+          tmpColor1 = image1[i][j]
+          #print('tempclr; ', len(tmpColor1))
+          tmpColor2 = image2[i][j]
+          red_diff = int(tmpColor1[0]) - int(tmpColor2[0])
+          green_diff = int(tmpColor1[1]) - int(tmpColor2[1])
+          blue_diff = int(tmpColor1[2]) - int(tmpColor2[2])
+          outputMap[i][j][0] = float(red_diff * red_diff)
+          outputMap[i][j][1] = float(green_diff * green_diff)
+          outputMap[i][j][2] = float(blue_diff * blue_diff)
 
     return outputMap
 
-def getELA(filename):
+def getELA(imageDirectory, imageName, outputDirectory):
+    filename = imageDirectory+imageName
+
     sc_width = 600
     sc_height = 600
-
+    
     quality = 75;
     displayMultiplier = 20;
 
     origImage = cv2.imread(filename)
-    outpath = "recompressed_" + filename
+    outpath = outputDirectory + 'recompressed_' + imageName
 
     cv2.imwrite(outpath, origImage, [int(cv2.IMWRITE_JPEG_QUALITY), quality])
     recompressedImage = cv2.imread(outpath)
@@ -48,12 +54,13 @@ def getELA(filename):
     displaySurface_temp = intDifference
 
     ds_height = displaySurface_temp.shape[1]
-    ds_width = displaySurface_temp.shape[2]
+    ds_width = displaySurface_temp.shape[0]
 
     if (ds_height > ds_width):
         if (ds_height > sc_height):
             sc_width = (sc_height * ds_width) / ds_height
-            displaySurface = cv2.resize(displaySurface_temp, (sc_width, sc_height))
+            displaySurface = cv2.resize(displaySurface_temp, (int(sc_width), sc_height))
+
         else:
             displaySurface = displaySurface_temp
     else:
@@ -62,10 +69,11 @@ def getELA(filename):
             displaySurface = cv2.resize(displaySurface_temp, (sc_width, sc_height))
         else:
             displaySurface = displaySurface_temp
-
-    cv2.imwrite("diff_" + filename, displaySurface)
+    print('ds: ', (displaySurface.shape))
+    displaySurface = displaySurface_temp
+    cv2.imwrite(outputDirectory + 'difference_' + imageName , displaySurface)
 
     return origImage
 
-if __name__ == "__main__":
-    getELA("comp30.jpg")
+#if __name__ == "__main__":
+ #   getELA("comp30.jpg")
